a41a79ec9440dfe8994d24ac91715b23825a8b15
leandromendes25 <leandromiendes@hotmail.com>
2026-04-22 15:50:40 -0300

    adicionando hero-section, features-section e a criação de csv-json-converter

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroSection } from './components/hero-section/hero-section';
+import { FeaturesSection } from './components/features-section/features-section';
+import { CsvJsonConverter } from './components/csv-json-converter/csv-json-converter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroSection],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('JSON2CSV');
-}
+export class App {}
---
+++ src/app/components/csv-json-converter/csv-json-converter.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-csv-json-converter',
+  imports: [],
+  templateUrl: './csv-json-converter.html',
+  styleUrl: './csv-json-converter.scss',
+})
+export class CsvJsonConverter {
+
+}
